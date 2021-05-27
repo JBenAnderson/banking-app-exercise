df411bb2ae08ca9a693afeb0b7afe4c5cbe86eb0
J Ben Anderson <revovisions@gmail.com>
2021-05-27 15:49:04 -0400

    added code within navbar.js and index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function Spa() {
+  return (
+    <>
+      <h1>Welcome to Front-End Bank</h1>
+      <h6>"Made with React and bootstrap!"</h6>
+      <NavBar />
+    </>
+  );
+}
+
+ReactDOM.render(<Spa />, document.getElementById("root"));
---
+++ navbar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <>
+      <a href="#">Bank</a>,<a href="#/CreateAccount/">Create Account</a>,
+      <a href="#/login/">Login</a>,<a href="#/deposit/">Deposit</a>,
+      <a href="#/withdraw/">Withdraw</a>,<a href="#/balance/">Balance</a>,
+    </>
+  );
+}
